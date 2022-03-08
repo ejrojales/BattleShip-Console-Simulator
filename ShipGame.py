7792f6f109475849cbfb8cee54a0e0b1b23cfc62
Emmanuel Rojales <ejrojales@gmail.com>
2022-03-08 17:26:56 +1000

    Docstrings and comments

--- ShipGame.py
+++ ShipGame.py
@@ -1,42 +1,35 @@
 # Author: Emmanuel Rojales
 # Github Username: ejrojales
-# Date: 2/28/2022
-# Description:
+# Date: 3/8/2022
+# Description: Program simulates the BattleShip game. Players are assigned and place ships on a 10x10 grid.
+# Players take turns firing torpedoes to sink sink the opponents ships.
+# A winner is declared once a player has lost all their ships.
 
 class Grid:
-    """Represents a player to be added to the ShipGame class."""
+    """Represents a 10x10 grid to be added to the ShipGame class."""
 
     def __init__(self, name):
-        """Initializes a player with a name and two grids, one for placing ships and one for keeping track of hits
-        and sunk ships. """
+        """
+        Initializes a 10x10 grid dedicated to one player. Tracks the number of ships placed and the coordinates
+        of each ship placement.
+
+        :param (str) name: Either 'first' or 'second'.
+        """
 
         self._name = name
         self._ship_placements = []
         self._num_of_ships = 0
         self._primary_grid = [[" ", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], ["A"], ["B"], ["C"], ["D"], ["E"], ["F"], ["G"],
                               ["H"], ["I"], ["J"]]
+
+        # Populate grid with empty space
         for i in range(1 ,11):
             for y in range(10):
                 self._primary_grid[i].append(" ")
-        # self._tracking_grid = [[" ", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], ["A"], ["B"], ["C"], ["D"], ["E"], ["F"], ["G"],
-        #                        ["H"], ["I"], ["J"]]
-        # for i in range(1 ,11):
-        #     for y in range(10):
-        #         self._tracking_grid[i].append(" ")
-
-    # def get_primary_grid(self):
-    #     """Return the player's grid with ships placed on it."""
-    #
-    #     return self._primary_grid
-
-    # def get_tracking_grid(self):
-    #     """Return the grid with a record of hits and sunk ships."""
-    #
-    #     return self._tracking_grid
 
     def set_ships(self, length, position, orientation):
         """
-        Places a ship on a player's primary grid. Call's the Player object's set_primary_grid().
+        Places a ship on a player's grid.
 
         :param (int) length: Length of ship.
         :param (str) position: Coordinate on board closest to A1 to place the ship.
@@ -48,10 +41,10 @@ class Grid:
         if length < 2 or length > 10:
             return False
 
+        # Translate position into index to access elements in primary grid.
         character = position[:1]
         letter = 0
         num = int(position[1:])
-
         for row in range(1, 11):
             if character in self._primary_grid[row]:
                 letter = row
@@ -67,6 +60,7 @@ class Grid:
                     return False
                 temp_letter += 1
 
+            # Update number of ships and ship placements
             self._num_of_ships += 1
             self._ship_placements.append([])
             # Place ship
@@ -88,9 +82,10 @@ class Grid:
                     return False
                 temp_num += 1
 
-            # Place ship
+            # Update number of ships and ship placements
             self._num_of_ships += 1
             self._ship_placements.append([])
+            # Place ship
             for ship in range(length):
                 self._primary_grid[letter][num] = "X"
                 self._ship_placements[len(self._ship_placements) - 1].append(f"{character}{num}")
@@ -102,7 +97,6 @@ class Grid:
         Updates the player's primary grid to reflect a hit on a ship.
 
         :param (str) position: The coordinate on the grid.
-        :return:
         """
 
         character = position[:1]
@@ -130,6 +124,8 @@ class Grid:
         return self._num_of_ships
 
     def display_grid(self):
+        """Display the grid"""
+
         for row in self._primary_grid:
             for col in row:
                 print(col, end=" ")
@@ -153,7 +149,7 @@ class ShipGame:
 
     def place_ship(self, player, length, position, orientation):
         """
-        Places a ship on a player's primary grid. Call's the Player object's set_primary_grid().
+        Places a ship on a player's primary grid.
 
         :param (str) player: Either 'first' or 'second'.
         :param (int) length: Length of ship.
@@ -166,12 +162,13 @@ class ShipGame:
         return self._players[player].set_ships(length, position, orientation)
 
     def get_current_state(self):
-        """"""
+        """Return the current state of the game."""
 
         return self._current_state
 
     def fire_torpedo(self, turn, position):
         """
+        Fires a torpedo on the opposing player's grid and updates the turn of the next player.
 
         :param (str) turn: Either 'first' or 'second'.
         :param (str) position: A coordinate on the grid.
@@ -181,9 +178,7 @@ class ShipGame:
         if turn != self._current_turn or self._current_state != 'UNFINISHED':
             return False
         else:
-
             winner = None
-
             for player in self._players:
                 if player != turn:
                     self._current_turn = player
@@ -200,7 +195,7 @@ class ShipGame:
 
     def get_num_ships_remaining(self, player):
         """
-        Return the total number of times a player has successfully called place_ship().
+        Return the total number of ships a player has remaining.
 
         :param (str) player: Either 'first' or 'second'.
         :return (int): The total number of ships placed.
